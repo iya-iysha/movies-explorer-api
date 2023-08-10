a5afd69d625e898fc115c4b6d054b116bf69ed76
Iya Vaksina <iya-iysha@mail.ru>
2023-08-10 10:42:07 +0300

    changed port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { errors } = require('celebrate');
 const { handleErrors } = require('./middlewares/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
