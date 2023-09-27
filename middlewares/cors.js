d0a2cbb8ef3f895b1cd8e8bd52d18a252028ab32
Iya Vaksina <iya-iysha@mail.ru>
2023-09-27 19:36:08 +0300

    fix cors

---
+++ middlewares/cors.js
@@ -0,0 +1,27 @@
+const allowedCors = [
+  'http://localhost:3000',
+  'https://movies-explorer.iya-iysha.nomoreparties.co',
+  'http://movies-explorer.iya-iysha.nomoreparties.co',
+];
+
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+module.exports.cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    res.status(200).send();
+    return;
+  }
+
+  next();
+};
